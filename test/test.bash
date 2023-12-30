8f8b085e29d073327e885c0cc0aca0022dbaefaf
RE <rhenium4694@gmail.com>
2023-12-31 03:00:16 +0900

    re  INPUT

--- test/test.bash
+++ test/test.bash
@@ -10,10 +10,10 @@ colcon build
 source $dir/.bashrc
 
 cd src/mypkg
-# cat inputs.txt | read inpu
-inpu = cat inputs.txt
+# cat inputs.txt | read INPUT
+INPUT = cat inputs.txt
 cd ../..
-(ros2 launch mypkg talk_listen.launch.py < $inpu ) > /tmp/mypkg.log
+(ros2 launch mypkg talk_listen.launch.py < $INPUT ) > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 1'
