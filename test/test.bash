f6ef1e7f4eb6c0549f41912740ffaa7229896880
RE <rhenium4694@gmail.com>
2023-12-31 03:06:15 +0900

    read while ~~ do ~~ done << log

--- test/test.bash
+++ test/test.bash
@@ -10,10 +10,10 @@ colcon build
 source $dir/.bashrc
 
 cd src/mypkg
-# cat inputs.txt | read INPUT
-INPUT = cat inputs.txt
-cd ../..
-(ros2 launch mypkg talk_listen.launch.py < $INPUT ) > /tmp/mypkg.log
+while read INPUT
+do
+ros2 launch mypkg talk_listen.launch.py < $INPUT 
+done > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 1'
